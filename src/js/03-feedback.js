23395323008f02b22568298ecd7747aa25dfa3ec
Tetjana0508 <dancingtatyana@gmail.com>
2023-04-02 23:36:17 +0300

    Д/з 8, частина 3 (зміни2)
    збірна коду

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,31 +7,37 @@ const STORAGE_KEY = 'feedback-form-state';
 const selectedFilters = {};
 
 const filterForm = document.querySelector('.feedback-form');
-const textareaEl = document.querySelector('.feedback-form textarea')
+// const textareaEl = document.querySelector('.feedback-form textarea')
 
+// textareaMessage();
 filterForm.addEventListener('submit', onFormSubmit);
-textareaEl.addEventListener('input', throttle(onTextareaInput, 500));
+// textareaEl.addEventListener('input', throttle(onTextareaInput, 500));
+filterForm.addEventListener('input', throttle(evt => {
+  selectedFilters[evt.target.name] = evt.target.value;
+  onTextareaInput();
+}, 500)
+);
 
-function onFormSubmit(evt) {
-  evt.preventDefault();
-  console.log('Отправляем форму');
-  evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+
+function onTextareaInput() { /* Функция onTextareaInput вызывается при вводе текста в поле и сохраняет значение в localStorage. */
+  // const message = evt.target.value;
+  // console.log(message);
+  // localStorage.setItem(STORAGE_KEY, message);
+  // localStorage.setItem(STORAGE_KEY, JSON.stringify(selectedFilters));
+
+//   const email = filterForm.elements.email.value;
+//   const message = filterForm.elements.message.value;
+//   const currentState = { email, message };
+// localStorage.setItem(STORAGE_KEY, JSON.stringify(currentState));
+const currentState = JSON.stringify(selectedFilters);
+localStorage.setItem(STORAGE_KEY, currentState);
 };
-// console.log(onFormSubmit);
 
-function onTextareaInput(evt) {
-  const message = evt.target.value;
-  console.log(message);
-  localStorage.setItem(STORAGE_KEY, message);
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(selectedFilters));
-};
-
-function populateTextarea() {
+function populateTextarea() { /* При загрузке страницы проверяем состояние хранилища и заполняем поля формы, если есть сохраненные данные */
   const savedMessage = localStorage.getItem(STORAGE_KEY);
   if (savedMessage) {
-    console.log(savedMessage);
-    textareaEl.value = savedMessage;
+    filterForm.elements.email.value = savedMessage || '';
+    filterForm.elements.message.value = savedMessage || '';
   }
 }
 populateTextarea();
@@ -40,8 +46,32 @@ filterForm.addEventListener('input', e => {
   selectedFilters[e.target.name] = e.target.value;
   console.log(selectedFilters);
 });
+/* В конце кода, еще один слушатель события добавляется на форму, который слушает изменения значений в полях формы и сохраняет их в объект selectedFilters. */
 
+function onFormSubmit(evt) { /* Функция onFormSubmit вызывается при отправке формы и отменяет ее действие по умолчанию, сбрасывает значения полей формы и удаляет данные из localStorage. */
+  evt.preventDefault();
+  console.log('Отправляем форму');
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 
+  // const email = filterForm.elements.email.value;
+  // const message = filterForm.elements.message.value;
+  // const formData = new FormData(filterForm);
+  // formData.forEach(email, message);
+  //   console.log({ email, message });
+  //   localStorage.removeItem(STORAGE_KEY);
+  // filterForm.reset();
+  
+  //   const formData {
+  //     elements: { email, message }
+  //   } = evt.currentTarget;
+  // console.log({ email: email.value, message: message.value });
+  // };
+  // evt.currentTarget.reset();
+  // localStorage.removeItem(STORAGE_KEY);
+  // // console.log(onFormSubmit);
+  // formData();
+};
 
 
 
@@ -49,16 +79,14 @@ filterForm.addEventListener('input', e => {
 
 // initForm();
 
-// filterForm.addEventListener('submit', evt => {
-//   evt.preventDefault();
-//   const formData = new FormData(filterForm);
-//   formData.forEach((value, name) => console.log(value, name));
-// });
 
 // filterForm.addEventListener('input', evt => {
 //   selectedFilters[evt.target.name] = evt.target.value;
 //   localStorage.setItem(STORAGE_KEY, JSON.stringify(selectedFilters));
 // });
+/* Второй блок кода добавляет обработчик события ввода ('input') на форму с идентификатором 'filterForm'. Когда пользователь вводит данные в любое поле формы, значение поля сохраняется в объект selectedFilters с именем поля в качестве ключа. Затем метод localStorage.setItem() используется для сохранения selectedFilters в локальном хранилище браузера с помощью ключа STORAGE_KEY.
+ */
+
 
 // function initForm() {
 //   let persistedFilters = localStorage.getItem(STORAGE_KEY);
@@ -71,3 +99,13 @@ filterForm.addEventListener('input', e => {
 //     })
 //   }
 // }
+/* Функция initForm() вызывается для проверки, сохранены ли выбранные пользователем фильтры в локальном хранилище браузера. Если они сохранены, persistedFilters распаковывается из строки JSON, сохраненной в локальном хранилище, и каждое значение добавляется в selectedFilters. Затем значения полей формы обновляются с помощью полученных сохраненных значений.
+ */
+
+// filterForm.addEventListener('submit', evt => {
+//   evt.preventDefault();
+//   const formData = new FormData(filterForm);
+//   formData.forEach((value, name) => console.log(value, name));
+// });
+/* Первый блок кода добавляет обработчик события отправки формы ('submit') на форму с идентификатором 'filterForm'. Обработчик предотвращает отправку формы (с помощью метода evt.preventDefault()) и создает объект FormData из отправленных данных формы. Затем метод forEach() используется для перебора данных формы и вывода каждого значения в консоль.
+ */
